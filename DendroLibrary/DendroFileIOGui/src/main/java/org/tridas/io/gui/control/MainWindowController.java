44508da686befa50424e261a7b09d1cb8d243db3
dmurphy10 <dmurphy10@843287a2-b704-49b0-8f52-e9cd02a0a56e>
2010-06-09 01:56:46 +0000

    about box has a different look, check it out

--- DendroLibrary/DendroFileIOGui/src/main/java/org/tridas/io/gui/control/MainWindowController.java
+++ DendroLibrary/DendroFileIOGui/src/main/java/org/tridas/io/gui/control/MainWindowController.java
@@ -33,7 +33,6 @@ public class MainWindowController extends FrontController {
 		view.setDefaultCloseOperation(3);
 		view.setVisible(true);
 		about = new AboutWindow(view);
-		about.pack();
 	}
 	
 	public void quit(MVCEvent argEvent) {
