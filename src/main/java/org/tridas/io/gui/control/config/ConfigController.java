0ed00429d30f960501f898ad132c3050e2104dde
Daniel Murphy <dmurph@users.noreply.github.com>
2010-06-16 20:36:04 +0000

    * added writer defaults editor * added gui error when can not find defaults object * added boolean default value editor

--- src/main/java/org/tridas/io/gui/control/config/ConfigController.java
+++ src/main/java/org/tridas/io/gui/control/config/ConfigController.java
@@ -3,8 +3,12 @@
  */
 package org.tridas.io.gui.control.config;
 
+import java.awt.Frame;
 import java.util.TreeMap;
 
+import javax.swing.JOptionPane;
+
+import org.tridas.io.AbstractDendroCollectionWriter;
 import org.tridas.io.AbstractDendroFileReader;
 import org.tridas.io.TridasIO;
 import org.tridas.io.defaults.AbstractDefaultValue;
@@ -27,6 +31,7 @@ public class ConfigController extends FrontController {
 	public static final String SET_INPUT_FORMAT = "CONFIG_SET_INPUT_FORMAT";
 	public static final String SET_OUTPUT_FORMAT = "CONFIG_SET_OUTPUT_FORMAT";
 	public static final String INPUT_DEFAULTS_PRESSED = "CONFIG_INPUT_DEFAULTS_PRESSED";
+	public static final String OUTPUT_DEFAULTS_PRESSED = "CONFIG_OUTPUT_DEFAULTS_PRESSED";
 	public static final String SET_NAMING_CONVENTION = "CONFIG_SET_NAMING_CONVENTION";
 	public static final String SET_READING_CHARSET = "CONFIG_SET_READING_CHARSET";
 	public static final String SET_WRITING_CHARSET = "CONFIG_SET_WRITING_CHARSET";
@@ -41,6 +46,7 @@ public class ConfigController extends FrontController {
 			registerCommand(SET_READING_CHARSET, "setReadingCharset");
 			registerCommand(SET_WRITING_CHARSET, "setWritingCharset");
 			registerCommand(INPUT_DEFAULTS_PRESSED, "displayInputDefaults");
+			registerCommand(OUTPUT_DEFAULTS_PRESSED, "displayOutputDefaults");
 		} catch (SecurityException e) {
 			e.printStackTrace();
 		} catch (NoSuchMethodException e) {
@@ -50,11 +56,13 @@ public class ConfigController extends FrontController {
 	
 	public void setInputFormat(MVCEvent argEvent) {
 		StringEvent event = (StringEvent) argEvent;
+		model.setReaderDefaults(null);
 		model.setInputFormat(event.getValue());
 	}
 	
 	public void setOutputFormat(MVCEvent argEvent) {
 		StringEvent event = (StringEvent) argEvent;
+		model.setWriterDefaults(null);
 		model.setOutputFormat(event.getValue());
 	}
 	
@@ -93,7 +101,9 @@ public class ConfigController extends FrontController {
 		if(model.getReaderDefaults() == null){
 			AbstractDendroFileReader reader = TridasIO.getFileReader(model.getInputFormat());
 			if(reader == null){
-				System.err.println("reader was null");
+				Frame parent = ModelLocator.getInstance().getMainWindow();
+				JOptionPane.showMessageDialog(parent, "Could not find defaults for input format '"+model.getInputFormat()+"'.",
+											  "Error", JOptionPane.ERROR_MESSAGE);
 				return;
 			}else{
 				model.setReaderDefaults(reader.constructDefaultMetadata());
@@ -106,6 +116,28 @@ public class ConfigController extends FrontController {
 		
 		MetadataEditor editor = new MetadataEditor(ModelLocator.getInstance().getMainWindow(), model);
 		editor.setVisible(true);
+	}
+	
+	public void displayOutputDefaults(MVCEvent argEvent){
+		MetadataTableModel tmodel = new MetadataTableModel();
 		
+		if(model.getWriterDefaults() == null){
+			AbstractDendroCollectionWriter writer = TridasIO.getFileWriter(model.getOutputFormat());
+			if(writer == null){
+				Frame parent = ModelLocator.getInstance().getMainWindow();
+				JOptionPane.showMessageDialog(parent, "Could not find defaults for output format '"+model.getOutputFormat()+"'.",
+											  "Error", JOptionPane.ERROR_MESSAGE);
+				return;
+			}else{
+				model.setWriterDefaults(writer.constructDefaultMetadata());
+			}
+		}
+		tmodel.setMetadataSet(model.getWriterDefaults());
+
+		MetadataEditorModel model = new MetadataEditorModel(I18n.getText("view.popup.meta.writerDefault"));
+		model.setTableModel(tmodel);
+		
+		MetadataEditor editor = new MetadataEditor(ModelLocator.getInstance().getMainWindow(), model);
+		editor.setVisible(true);
 	}
 }
