dde7fc40d705a5d4c1fe01ba390ee487acf83d8b
dmurphy10 <dmurphy10@843287a2-b704-49b0-8f52-e9cd02a0a56e>
2010-06-25 15:09:09 +0000

    added boilerplate license to each file

--- DendroLibrary/DendroFileIOGui/src/main/java/org/tridas/io/gui/components/CustomTreeCellRenderer.java
+++ DendroLibrary/DendroFileIOGui/src/main/java/org/tridas/io/gui/components/CustomTreeCellRenderer.java
@@ -1,4 +1,19 @@
 /**
+ * Copyright 2010 Daniel Murphy
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *   
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
  * Created at Jun 13, 2010, 1:13:41 PM
  */
 package org.tridas.io.gui.components;
@@ -10,7 +25,6 @@ import javax.swing.JTree;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeCellRenderer;
 
-import org.apache.commons.lang.WordUtils;
 import org.tridas.io.gui.I18n;
 import org.tridas.io.gui.control.convert.ConvertController.DendroWrapper;
 import org.tridas.io.gui.control.convert.ConvertController.StructWrapper;
--- DendroLibrary/DendroFileIOGui/src/main/java/org/tridas/io/gui/components/editors/IntegerEditor.java
+++ DendroLibrary/DendroFileIOGui/src/main/java/org/tridas/io/gui/components/editors/IntegerEditor.java
@@ -1,11 +1,25 @@
 /**
+ * Copyright 2010 Daniel Murphy
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *   
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
  * Created at Jun 16, 2010, 3:20:32 AM
  */
 package org.tridas.io.gui.components.editors;
 
 import java.awt.Component;
 
-import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 import org.tridas.io.defaults.values.IntegerDefaultValue;
@@ -16,6 +30,7 @@ import org.tridas.io.gui.components.DefaultFieldEditor.AbstractEditorDelegate;
  * @author daniel
  *
  */
+@SuppressWarnings("serial")
 public class IntegerEditor extends AbstractEditorDelegate{
 
 	public JTextField comp = new JTextField();
