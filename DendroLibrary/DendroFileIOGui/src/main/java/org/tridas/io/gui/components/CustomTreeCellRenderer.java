044a530850b44266f64bce1b760780427e8d20cc
dmurphy10 <dmurphy10@843287a2-b704-49b0-8f52-e9cd02a0a56e>
2010-07-09 06:39:12 +0000

    fixed no text in the tree fixed cancel buttons

--- DendroLibrary/DendroFileIOGui/src/main/java/org/tridas/io/gui/components/CustomTreeCellRenderer.java
+++ DendroLibrary/DendroFileIOGui/src/main/java/org/tridas/io/gui/components/CustomTreeCellRenderer.java
@@ -59,7 +59,8 @@ public class CustomTreeCellRenderer extends DefaultTreeCellRenderer {
 	@Override
 	public Component getTreeCellRendererComponent(JTree tree, Object value, boolean sel, boolean expanded,
 			boolean leaf, int row, boolean hasFocus) {
-		//Component def = super.getTreeCellRendererComponent(tree, value, sel, expanded, leaf, row, hasFocus);
+		// need this call to populate text
+		super.getTreeCellRendererComponent(tree, value, sel, expanded, leaf, row, hasFocus);
 		
 		Object userObject = ((DefaultMutableTreeNode)value).getUserObject();
 		if(userObject instanceof StructWrapper){
