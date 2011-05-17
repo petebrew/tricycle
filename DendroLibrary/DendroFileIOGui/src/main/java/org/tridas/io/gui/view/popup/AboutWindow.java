6faed6d82c46d05eb275a0a0f465262e0a6656b8
sbr00pwb <sbr00pwb@843287a2-b704-49b0-8f52-e9cd02a0a56e>
2011-05-17 17:54:49 +0000

    Preparing for new release

--- DendroLibrary/DendroFileIOGui/src/main/java/org/tridas/io/gui/view/popup/AboutWindow.java
+++ DendroLibrary/DendroFileIOGui/src/main/java/org/tridas/io/gui/view/popup/AboutWindow.java
@@ -18,7 +18,9 @@
  */
 package org.tridas.io.gui.view.popup;
 
-import java.awt.Dimension;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -28,8 +30,11 @@ import javax.swing.ImageIcon;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JScrollPane;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
 import javax.swing.JTextArea;
+
+import net.miginfocom.swing.MigLayout;
 
 import org.tridas.io.gui.App;
 import org.tridas.io.gui.I18n;
@@ -42,7 +47,9 @@ import org.tridas.io.util.IOUtils;
 @SuppressWarnings("serial")
 public class AboutWindow extends JDialog {
 	
-	private JTextArea info;
+	private JLabel txtVersion;
+	private JLabel txtTimestamp;
+	private JLabel txtRevision;
 	
 	public AboutWindow(JFrame argParent) {
 		super(argParent, true);
@@ -59,17 +66,86 @@ public class AboutWindow extends JDialog {
 	 */
 	private void initComponents() {
 		
-		JLabel llogo = new JLabel(new ImageIcon(IOUtils.getFileInJarURL("icons/256x256/application.png")));
-		
-		info = new JTextArea();
-		info.setEditable(false);
-		
 		Box box = Box.createHorizontalBox();
 		box.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-		box.add(llogo);
-		box.add(Box.createRigidArea(new Dimension(5, 1)));
-		box.add(new JScrollPane(info));
-		add(box, "Center");
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		box.add(tabbedPane);
+		
+		JPanel panelAbout = new JPanel();
+		tabbedPane.addTab(I18n.getText("view.popup.about"), null, panelAbout, null);
+		panelAbout.setLayout(new MigLayout("", "[171.00px,grow][grow][]", "[][][][][][][][][][109.00px,grow,fill]"));
+		
+		JLabel lblTitle = new JLabel(I18n.getText("view.popup.about.header"));
+		lblTitle.setFont(new Font("Dialog", Font.BOLD, 16));
+		panelAbout.add(lblTitle, "cell 0 0 3 1,alignx center");
+		
+		JLabel llogo = new JLabel(new ImageIcon(IOUtils.getFileInJarURL("icons/64x64/application.png")));
+		panelAbout.add(llogo, "cell 2 1 1 5,alignx center,aligny top");
+		
+		JLabel lblVersion = new JLabel(I18n.getText("view.popup.about.version")+":");
+		panelAbout.add(lblVersion, "cell 0 2,alignx right");
+		
+		txtVersion = new JLabel("x.x.x");
+		txtVersion.setFont(new Font("Dialog", Font.PLAIN, 12));
+		panelAbout.add(txtVersion, "cell 1 2");
+		
+		JLabel lblBuildRevision = new JLabel(I18n.getText("view.popup.about.revision")+":");
+		panelAbout.add(lblBuildRevision, "cell 0 3,alignx right");
+		
+		txtRevision = new JLabel("xxx");
+		txtRevision.setFont(new Font("Dialog", Font.PLAIN, 12));
+		panelAbout.add(txtRevision, "cell 1 3");
+		
+		JLabel lblBuildTimestamp = new JLabel(I18n.getText("view.popup.about.timestamp")+":");
+		panelAbout.add(lblBuildTimestamp, "cell 0 4,alignx right");
+		
+		txtTimestamp = new JLabel("xx Xxx 20XX, xx:xx");
+		txtTimestamp.setFont(new Font("Dialog", Font.PLAIN, 12));
+		panelAbout.add(txtTimestamp, "cell 1 4");
+		
+		JLabel lblDendroiolibraryBy = new JLabel(I18n.getText("view.popup.about.libby")+":");
+		panelAbout.add(lblDendroiolibraryBy, "cell 0 6,alignx right");
+		
+		JLabel txtLibAuthors = new JLabel(I18n.getText("view.popup.about.libauthors"));
+		txtLibAuthors.setFont(new Font("Dialog", Font.PLAIN, 12));
+		panelAbout.add(txtLibAuthors, "cell 1 6 2 1");
+		
+		JLabel lblGraphicalInterfaceBy = new JLabel(I18n.getText("view.popup.about.guiby")+":");
+		panelAbout.add(lblGraphicalInterfaceBy, "cell 0 7,alignx right");
+		
+		JLabel txtGUIAuthors = new JLabel(I18n.getText("view.popup.about.guiauthors"));
+		txtGUIAuthors.setFont(new Font("Dialog", Font.PLAIN, 12));
+		panelAbout.add(txtGUIAuthors, "cell 1 7 2 1");
+		
+		JPanel panel = new JPanel();
+		panelAbout.add(panel, "cell 0 9 3 1,grow");
+		panel.setLayout(new BorderLayout(0, 0));
+		
+		JTextArea txtAcknowledgements = new JTextArea();
+		txtAcknowledgements.setForeground(Color.BLACK);
+		txtAcknowledgements.setEnabled(false);
+		txtAcknowledgements.setEditable(false);
+		txtAcknowledgements.setLineWrap(true);
+		txtAcknowledgements.setWrapStyleWord(true);
+		txtAcknowledgements.setText(I18n.getText("view.popup.about.acknowledgementstext"));
+		
+		panel.add(txtAcknowledgements);
+		
+		JLabel lblAcknowledgements = new JLabel(I18n.getText("view.popup.about.acknowledgements")+":");
+		panel.add(lblAcknowledgements, BorderLayout.NORTH);
+			
+		
+		JPanel panelLicense = new JPanel();
+		tabbedPane.addTab(I18n.getText("view.popup.about.license"), null, panelLicense, null);
+		panelLicense.setLayout(new BorderLayout(0, 0));
+		
+		JTextArea txtLicense = new JTextArea();
+		txtLicense.setText(I18n.getText("view.popup.about.licensetext"));
+		txtLicense.setFont(new Font("Monospaced", Font.PLAIN, 12));
+		txtLicense.setEditable(false);
+		panelLicense.add(txtLicense, BorderLayout.CENTER);
+		getContentPane().add(box, "Center");
 	}
 	
 	
@@ -96,13 +172,12 @@ public class AboutWindow extends JDialog {
 		setIconImage(TricycleModelLocator.getInstance().getWindowIcon().getImage());
 		
 		// Extract build timestamp from Manifest
-		String timestamp = App.getBuildTimestamp();
-		String version = App.getBuildVersion();
-		String buildrevision = App.getBuildRevision();
-
-
+		txtVersion.setText(App.getBuildVersion());
+		txtRevision.setText(App.getBuildRevision());
+		txtTimestamp.setText(App.getBuildTimestamp());
+		
 
 		setTitle(I18n.getText("view.popup.about.title"));
-		info.setText(I18n.getText("view.popup.about.text", timestamp, version, buildrevision));
+		
 	}
 }
